096200a4319d361475823e694337377a2f685b7e
Bee-Ene <69353437+Bee-Ene@users.noreply.github.com>
2024-08-28 13:38:26 +0100

    Update app_recommender.py.py

--- PycharmProjects/Recommender_system/deployment/app_recommender.py.py
+++ PycharmProjects/Recommender_system/deployment/app_recommender.py.py
@@ -6,7 +6,6 @@ import os
 # Print current working directory
 print("Current Working Directory:", os.getcwd())
 
-# Define the file path (adjust as necessary)
 file_path = 'C:/Users/Heckerbella/PycharmProjects/Recommender_system/deployment/movies_dict.pkl'
 
 # Load the data
@@ -14,7 +13,7 @@ if os.path.exists(file_path):
     movies_dict = pickle.load(open(file_path, 'rb'))
 else:
     st.error("The file 'movies_dict.pkl' was not found.")
-    movies_dict = {}  # Or handle the missing file case appropriately
+    movies_dict = {}  #handle missing error file more properly
 
 movies = pd.DataFrame(movies_dict)
 similarity = pickle.load(open('C:/Users/Heckerbella/PycharmProjects/Recommender_system/deployment/similarity.pkl', 'rb'))
@@ -22,14 +21,14 @@ similarity = pickle.load(open('C:/Users/Heckerbella/PycharmProjects/Recommender_
 def recommender(movie):
     index = movies[movies['title'] == movie].index[0]
     dist = sorted(list(enumerate(similarity[index])), reverse=True, key=lambda x:x[1])
-    # top five similar movies
+    #recommend top five similar movies
 
     recommended_movies = [] 
     for i in dist[1:6]:
         recommended_movies.append(movies.iloc[i[0]].title)
     return recommended_movies
 
-# Streamlit application code
+# Streamlit application 
 st.title('Movie Recommender System')
 option = st.selectbox('Choose your movie:', movies['title'].values)
 
